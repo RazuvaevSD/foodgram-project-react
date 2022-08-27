e6ffca06b1350562de6ca66549ffd512caeadfc3
Сергей Разуваев <razuvaev.sd@ya.ru>
2022-08-27 17:04:41 +1100

    paginator fix

--- backend/foodgram/api/paginators.py
+++ backend/foodgram/api/paginators.py
@@ -3,5 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class LimitPagePagination(PageNumberPagination):
     """Фильтр пагинатора."""
-    page_size = 10
     page_size_query_param = 'limit'
