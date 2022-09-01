964e2a20ee12c0fe747123756335792cb7ba9201
Сергей Разуваев <razuvaev.sd@ya.ru>
2022-09-01 21:49:31 +1100

    upd workflow

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -72,12 +72,11 @@ DATABASES = {
     }
 }
 
-
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/backend_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static/')
 
 MEDIA_URL = '/backend_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/backend_media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media/')
 
 
 AUTH_PASSWORD_VALIDATORS = [
